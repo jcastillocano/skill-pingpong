dc611d3887a99cc2fbfa1f41fb4d1bbf50f6a507
juancarlos.castillo <juancarlos.castillo@system73.com>
2019-10-13 12:16:11 +0100

    Renamed PingPong Skill and added license

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ from opsdroid.skill import Skill
 from opsdroid.matchers import match_regex
 
 
-class PingPong(Skill):
+class PingPongSkill(Skill):
 
-    @match_regex('ping')
+    @match_regex(r'ping|ping!')
     async def ping_pong(self, message):
         await message.respond('pong!')
